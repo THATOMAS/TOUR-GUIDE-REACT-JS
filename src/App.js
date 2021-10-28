ce3ab0e8ee7002a718d30f310ddb1483af087c68
THATOMAS <chachumashori@gmail.com>
2021-10-28 11:57:05 +0200

    Added a few JS files to make the app more developer friendly,modified the CSS file to give the website its basic skeleton styling .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Loading from "./Loading"
+import Tours from "./Tour"
+
+import "./index.css"
+
+const url = 'https://course-api.com/react-tours-project'
+
+
 const App = () => {
 
 
+
   return <>
-  </>
+  <main>
+    <h2>Tours</h2>
+  </main>
+</>
 
 }
 
